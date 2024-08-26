d2caa07aa1f862d1adce0a069fafe0f7ec54c50a
ArtemKorole <artemrodilsya24092009@gmail.com>
2024-08-27 01:23:07 +0300

    Добавьте файлы проекта.

---
+++ build/CMakeFiles/FileManager.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/CommandParser.cpp" "CMakeFiles/FileManager.dir/source/CommandParser.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/CommandParser.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/DirEntity.cpp" "CMakeFiles/FileManager.dir/source/DirEntity.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/DirEntity.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/Directory.cpp" "CMakeFiles/FileManager.dir/source/Directory.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/Directory.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/File.cpp" "CMakeFiles/FileManager.dir/source/File.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/File.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/FileEntity.cpp" "CMakeFiles/FileManager.dir/source/FileEntity.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/FileEntity.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/FileReader.cpp" "CMakeFiles/FileManager.dir/source/FileReader.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/FileReader.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/FileWriter.cpp" "CMakeFiles/FileManager.dir/source/FileWriter.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/FileWriter.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/Shell.cpp" "CMakeFiles/FileManager.dir/source/Shell.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/Shell.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/common_functions.cpp" "CMakeFiles/FileManager.dir/source/common_functions.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/common_functions.cpp.obj.d"
+  "C:/Users/Королёв PC/Desktop/c++/FileManager/source/main.cpp" "CMakeFiles/FileManager.dir/source/main.cpp.obj" "gcc" "CMakeFiles/FileManager.dir/source/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/FileManager.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileManager.dir/source/CommandParser.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/CommandParser.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/DirEntity.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/DirEntity.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/Directory.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/Directory.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/File.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/File.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/FileEntity.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/FileEntity.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/FileReader.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/FileReader.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/FileWriter.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/FileWriter.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/Shell.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/Shell.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/common_functions.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/common_functions.cpp.obj.d"
+  "CMakeFiles/FileManager.dir/source/main.cpp.obj"
+  "CMakeFiles/FileManager.dir/source/main.cpp.obj.d"
+  "FileManager.exe"
+  "FileManager.exe.manifest"
+  "FileManager.pdb"
+  "libFileManager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
